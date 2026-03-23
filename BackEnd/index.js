3bdbef8ee86d5aba2b608805dbe6c145402293ce
ANDUS25 <anandchavan113@gmail.com>
2026-03-23 23:39:06 +0530

    Added the utils file

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,10 @@
 import connectToDatabase from "./db.js";
 import { app } from "./route.js";
+import { Title } from "./utils/stings.js";
 
 (async function () {
   app.listen(process.env.PORT, async () => {
-    console.log(`server running on port ${process.env.PORT}`);
+    console.log(`${Title.SERVER_RUNNING_ON_PORT} ${process.env.PORT}`);
     await connectToDatabase();
   });
 })();
